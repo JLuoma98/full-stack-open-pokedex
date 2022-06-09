0fbb7a027b5771d29ab9b09f6d9fa03a38c0950b
Jere Luoma <jere.luoma@gmail.com>
2022-06-09 11:31:35 +0300

    11.10

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.use(express.static('dist'))
-//
+
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
